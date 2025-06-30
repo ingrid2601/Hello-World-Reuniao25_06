d8e3ec754b69d98e9a8e010ea0ed01162418832c
ingrid2601 <159648748+ingrid2601@users.noreply.github.com>
2025-06-30 15:34:02 -0300

    .

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ function inserirNome(){
 
 inserirNome();
 
-//Lista de linguagens escritas:
-const linguagens =["JavaScript", "Python","C++"]
+const linguagens = ["JavaScript", "Python","C++"]
 
 console.log (linguagens [0]) 
 console.log (linguagens [1]) 
 console.log (linguagens [2]) 
 
-const lista = document.querySelector(#lista");
+const lista = document.querySelector("#lista");
 lista.textContent = linguagens [0];
 lista.textContent = linguagens [1];
 lista.textContent = linguagens [2];
