fb29c9e6799400764eb09487dad318b0543e1f1a
ingrid2601 <159648748+ingrid2601@users.noreply.github.com>
2025-06-30 15:03:37 -0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function inserirNome(){
    let nomeUsuario = prompt("Qual o seu nome");
    let elemento = document.querySelector("#nome-usuario");
-   console.log(elemento);
    elemento.textContent = nomeUsuario;
 }
 
 inserirNome();
+
+//Lista de linguagens escritas:
+const linguagens =["JavaScript", "Python","C++"]
+
+console.log (linguagens [0]) //"JavaScript"
+console.log (linguagens [1]) //"Phyton"
+console.log (linguagens [2]) //"C++"
+
